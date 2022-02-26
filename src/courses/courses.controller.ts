3767e11e19f43b4ff6a97a6fd64b84a083af5293
Euclides <euclideskinto@gmail.com>
2022-02-26 12:14:53 -0300

    add mapped-types, class-validator and class-transformer

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -8,6 +8,8 @@ import {
   Post,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
 
 @Controller('courses')
 export class CoursesController {
@@ -23,13 +25,13 @@ export class CoursesController {
   }
 
   @Post()
-  create(@Body() body: any) {
-    return this.coursesService.cerate(body);
+  create(@Body() createCourseDto: CreateCourseDto) {
+    return this.coursesService.cerate(createCourseDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return this.coursesService.update(id, body);
+  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+    return this.coursesService.update(id, updateCourseDto);
   }
 
   @Delete(':id')
---
+++ src/courses/dto/update-course.dto.ts
@@ -0,0 +1,3 @@
+import { CreateCourseDto } from './create-course.dto';
+import { PartialType } from '@nestjs/mapped-types';
+export class UpdateCourseDto extends PartialType<CreateCourseDto> {}
